82dc2775257aed59f8f70adbacbeb63d185f516d
Will Taff <wrtaff@users.noreply.github.com>
2015-02-17 11:39:56 -0500

    commit all files in etc less hopefully gitignore

---
+++ acpi/power.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+test -f /usr/share/acpi-support/key-constants || exit 0
+
+. /usr/share/acpi-support/power-funcs
+. /usr/share/acpi-support/policy-funcs
+
+if [ -z "$*" ] && ( [ `CheckPolicy` = 0 ] || CheckUPowerPolicy ); then
+    exit;
+fi
+
+pm-powersave $*
---
+++ mediawiki/AdminSettings.php
@@ -0,0 +1,13 @@
+#COPIED FROM http://www.mediawiki.org/wiki/Manual:AdminSettings.php
+#WRT: I had to create this in order to use dumpBackup.php.  The
+#Typical installation doesn't install it, and you need the variables
+#below for dumpBackup to work. 
+
+<?php
+/* Database authentication credentials */
+$wgDBadminuser      = 'root';
+$wgDBadminpassword  = 'wELnEWmIN13!!';
+ 
+/* Whether to enable the profileinfo.php script. */
+$wgEnableProfileInfo = false;
+
